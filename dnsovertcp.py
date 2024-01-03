3c2531caca94df77c6d52ef4ebf9bccbfc7d8588
Frank Seifferth <frankseifferth@posteo.net>
2024-01-03 20:57:06 +0100

    Make code more compact

--- dnsovertcp.py
+++ dnsovertcp.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-import logging
-import socket
-import struct
-
+import os, socket, struct
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
 class DNSHandler(DatagramProtocol):
-
-    """UDP Handler """
-
     dns_servers = ['127.0.0.1']
     timeout   = 3
     max_cache_size = 1500
     cache = {}
 
     def resolv_by_tcp(self,data):
-
         for dns_server in self.dns_servers:
             sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
             try:
@@ -26,12 +18,9 @@ class DNSHandler(DatagramProtocol):
                 sock.connect((dns_server,53))
                 sock.send(data)
                 data = sock.recv(512)
-
                 if len(data) < 10:
                     raise 'Failt to receive data'
-
                 return data[4:]
-
             except (IOError,socket.error,Exception) as e:
                 pass
             finally:
@@ -40,19 +29,14 @@ class DNSHandler(DatagramProtocol):
         return None
 
     def datagramReceived(self, data, address):
-
         cache   = self.cache
         timeout = self.timeout
-
         if len(cache) > self.max_cache_size:
             cache.clear()
-
         reqid   = data[:2]
         domain  = data[12:data.find(b'\x00', 12)]
-
         if domain in cache:
             return self.transport.write(reqid + cache[domain],address)
-
         sdata = b'%s\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00%s\x00\x00\x01\x00\x01' % (os.urandom(2), domain)
         sdata = struct.pack(b'>H',len(sdata)) + sdata
         rdata = self.resolv_by_tcp(sdata)
@@ -66,20 +50,10 @@ class DNSHandler(DatagramProtocol):
                list_domain[i]='.'
                i += domain[i]+1
 
-            logging.error('DNSServer failed to resolve %s',''.join(list_domain[1:]))
+            print('DNSServer failed to resolve %s',''.join(list_domain[1:]))
             rdata = '\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00'
-
         self.transport.write(reqid + rdata, address)
 
-
-class DNSServer():
-    def __init__(self, conf):
-        pass
-
-    def run(self):
-        reactor.listenUDP(53, DNSHandler(),interface='127.0.0.1')
-        reactor.run()
-
-
 if __name__ == '__main__':
-    DNSServer(None).run()
+    reactor.listenUDP(53, DNSHandler(),interface='127.0.0.1')
+    reactor.run()
