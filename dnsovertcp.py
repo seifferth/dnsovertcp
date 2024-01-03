694d7b6947b85c4c58e513f30ec0eb6418f4c367
Frank Seifferth <frankseifferth@posteo.net>
2024-01-03 20:52:20 +0100

    Remove whitespace at the end of lines

--- dnsovertcp.py
+++ dnsovertcp.py
@@ -20,27 +20,27 @@ class DNSHandler(DatagramProtocol):
     def resolv_by_tcp(self,data):
 
         for dns_server in self.dns_servers:
-            sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)        
+            sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
             try:
                 sock.settimeout(self.timeout)
                 sock.connect((dns_server,53))
                 sock.send(data)
                 data = sock.recv(512)
 
-                if len(data) < 10:                    
+                if len(data) < 10:
                     raise 'Failt to receive data'
-                
+
                 return data[4:]
 
             except (IOError,socket.error,Exception) as e:
                 pass
             finally:
                 if sock:
-                   sock.close()        
+                   sock.close()
         return None
-    
+
     def datagramReceived(self, data, address):
-        
+
         cache   = self.cache
         timeout = self.timeout
 
@@ -56,8 +56,8 @@ class DNSHandler(DatagramProtocol):
         sdata = b'%s\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00%s\x00\x00\x01\x00\x01' % (os.urandom(2), domain)
         sdata = struct.pack(b'>H',len(sdata)) + sdata
         rdata = self.resolv_by_tcp(sdata)
-        if rdata:            
-            cache[domain] = rdata        
+        if rdata:
+            cache[domain] = rdata
         else:
             #No result for this domain
             list_domain = list(domain)
@@ -68,17 +68,17 @@ class DNSHandler(DatagramProtocol):
 
             logging.error('DNSServer failed to resolve %s',''.join(list_domain[1:]))
             rdata = '\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00'
-            
-        self.transport.write(reqid + rdata, address)   
+
+        self.transport.write(reqid + rdata, address)
 
 
 class DNSServer():
-    def __init__(self, conf):        
+    def __init__(self, conf):
         pass
 
     def run(self):
         reactor.listenUDP(53, DNSHandler(),interface='127.0.0.1')
-        reactor.run()  
+        reactor.run()
 
 
 if __name__ == '__main__':
