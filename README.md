3930c24761f18fe8537c0bf11c243a63ac32d123
Frank Seifferth <frankseifferth@posteo.net>
2024-01-20 14:38:10 +0100

    Add some basic command line options

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 Transform the local dns udp requests to tcp requests. This script is based
 on an earlier version found at <https://github.com/dsheng/dnsovertcp>.
 
-`dnsovertcp.py` is a standalone python script that will listen on
-localhost port 53/udp in order to forward all dns requests to localhost
-port 53/tcp. To attach port 53/tcp to an actual dns server, you could
-use a tool like `socat`, or you could use `ssh -L` to reroute all dns
-requests through an ssh tunnel.
+`dnsovertcp.py` is a standalone python script that will listen on a
+local udp port and forward all dns requests to an upstream dns server
+via tcp. The default behaviour of `dnsovertcp.py` is to forward all dns
+requests from localhost 53/udp to localhost 53/tcp. Both ports as well as
+the upstream server's address can be overridden by command line arguments
+(see `./dnsovertcp.py --help` for further information). Furthermore, you
+could use a tool like `socat` to accommodate even more complex rerouting
+schemes, or you could use `ssh -L` to forward all upstream dns requests
+through an ssh tunnel.
 
 To check whether this script is working as intended, you might want to run
 something like
--- dnsovertcp.py
+++ dnsovertcp.py
@@ -3,14 +3,20 @@
 import os, sys, socket, struct
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+from getopt import gnu_getopt as getopt
 
 class DNSHandler(DatagramProtocol):
-    timeout = 3
+    def __init__(self, upstream: str='127.0.0.1', upstream_port: int=52,
+                 timeout: int=3):
+        self.upstream = upstream
+        self.upstream_port = upstream_port
+        self.timeout = timeout
+        super().__init__()
     def resolv_by_tcp(self,data):
         sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         try:
             sock.settimeout(self.timeout)
-            sock.connect(('127.0.0.1', 53))
+            sock.connect((self.upstream, self.upstream_port))
             sock.send(data)
             data = sock.recv(512)
             if len(data) < 10:
@@ -32,7 +38,41 @@ class DNSHandler(DatagramProtocol):
             print('DNSServer failed to resolve', domain, file=sys.stderr)
         self.transport.write(reqid + rdata, address)
 
+_cli_help = """
+Usage: dnsovertcp [-l PORT|-w TIMEOUT|-h] [DESTINATION [PORT]]
+
+Positional Arguments:
+    DESTINATION           Domain name or ip address of the upstream dns
+                          server. (default: 127.0.0.1)
+    PORT                  TCP port to connect to for dns requests against
+                          the upstream server. (default: 53)
+Further Options:
+    -l PORT,              Local udp port to open for incoming dns requests.
+        --listen PORT     (default: 53)
+    -w TIMEOUT,           Maximum number of seconds to wait for the upstream
+        --wait TIMEOUT    dns server to answer a query. (default: 3)
+    -h, --help            Print this help message and exit.
+""".lstrip()
+
 if __name__ == '__main__':
-    reactor.listenUDP(53, DNSHandler(), interface='127.0.0.1')
-    reactor.listenUDP(53, DNSHandler(), interface='::1')
+    opts, args = getopt(sys.argv[1:], 'l:w:h', ['listen=', 'wait=', 'help'])
+    listen_port = 53; timeout = 3; upstream = '127.0.0.1'; upstream_port = 53
+    for k, v in opts:
+        if k in ['-l', '--listen']:     listen_port = int(v)
+        elif k in ['-w', '--wait']:     timeout = int(v)
+        elif k in ['-h', '--help']:     print(_cli_help); exit(0)
+    if len(args) > 2:
+        print("Too many positional arguments", file=sys.stderr)
+        exit(1)
+    elif len(args) >= 1:
+        upstream = args[0]
+        if len(args) == 2: upstream_port = int(args[1])
+    reactor.listenUDP(listen_port, DNSHandler(upstream = upstream,
+                                              upstream_port = upstream_port,
+                                              timeout = timeout),
+                      interface='127.0.0.1')
+    reactor.listenUDP(listen_port, DNSHandler(upstream = upstream,
+                                              upstream_port = upstream_port,
+                                              timeout = timeout),
+                      interface='::1')
     reactor.run()
